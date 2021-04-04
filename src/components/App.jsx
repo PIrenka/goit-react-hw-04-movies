bed91c4bce5b6e8371dc057ed51d46fadd322a87
Iryna Protsenko <irene.protsenko@gmail.com>
2021-04-04 23:37:34 +0300

    init, and it could be used for import

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+
+// import Comp1 from './Comp1'
+
+function App() {
+  return <div className="App">PUT_HERE_YOUR_RENDER_DATAS_AND_TAGS</div>;
+  // <Comp1 />;
+}
+
+export default App;
---
+++ src/components/Comp1/Comp1.jsx
@@ -0,0 +1,6 @@
+// import React, { Component } from 'react'
+// import PropTypes from 'prop-types';
+
+// import Styles from "./stylesComp1.module.css";
+// const Comp1=()=>(...)
+// export default Comp1
