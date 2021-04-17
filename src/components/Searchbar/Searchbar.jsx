f2d44e77e00037b0768a537e4f40a72554216a8c
Iryna Protsenko <irene.protsenko@gmail.com>
2021-04-17 16:02:17 +0300

    add: Searchbar without rendering

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,43 @@
+import { Component } from 'react';
+import styles from './stylesSearchbar.module.scss';
+
+class Searchbar extends Component {
+  state = {
+    searchQuery: '',
+    movies: [],
+  };
+
+  handelChange = ev => {
+    this.setState({ searchQuery: ev.currentTarget.value });
+  };
+
+  handelSubmit = ev => {
+    ev.preventDefault();
+    this.props.onSubmit(this.state.searchQuery);
+    this.setState({ searchQuery: '' });
+  };
+
+  render() {
+    return (
+      <div className={styles.Searchbar}>
+        <form className={styles.SearchForm} onSubmit={this.handelSubmit}>
+          <button type="submit" className={styles.SearchForm_button}>
+            <span className={styles.SearchForm_button_label}>Search</span>
+          </button>
+
+          <input
+            className={styles.SearchForm_input}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            value={this.state.searchQuery}
+            onChange={this.handelChange}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Searchbar;
--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,2 +1,40 @@
-const MoviesPage = () => <h1>lets find some movies to watch</h1>;
-export default MoviesPage
+import { Component } from 'react';
+import Searchbar from '../../components/Searchbar';
+
+class MoviesPage extends Component {
+  state = {
+    movies: [],
+    searchQuery: '',
+  };
+
+  addMovies = query => {
+    this.setState({
+      searchQuery: query,
+      movies: [],
+      //   currentPage: 1,
+      //   error: null,
+    });
+  };
+
+  render() {
+    const {
+      showModal,
+      images,
+      isLoading,
+      searchQuery,
+      modalURL,
+      error,
+    } = this.state;
+
+    return (
+      <div>
+        {/* <Container> */}
+        <h1>here you can find more movies, just put the request</h1>
+        {/* </Container> */}
+
+        <Searchbar onSubmit={this.addMovies} />
+      </div>
+    );
+  }
+}
+export default MoviesPage;
